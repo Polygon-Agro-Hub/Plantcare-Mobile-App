1257447e6a60f4bdb412ab5a53fd5d5fb118d8bf
R.H.Pavith Madushan Rajapaksha <pavithmadushan@gmail.com>
2024-09-27 16:24:35 +0530

    unnecessary files deleted

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/_sitemap`;
+      StaticRoutes: `/` | `/..\Items\SinhalaCropItem` | `/..\Items\SinhalaCropSelectCard` | `/..\Items\SinhalaNavigationBar` | `/..\Items\SinhalaNewsSlideShow` | `/..\Items\TamilCropItem` | `/..\Items\TamilCropSelectCard` | `/..\Items\TamilNavigationBar` | `/..\Items\TamilNewsSlideShow` | `/..\component\AddAsset` | `/..\component\AddFixedAsset` | `/..\component\AssertsFixedView` | `/..\component\CropCalander` | `/..\component\CurrentAssert` | `/..\component\Dashbord` | `/..\component\EngEditProfile` | `/..\component\EngProfile` | `/..\component\EngQRcode` | `/..\component\FiveDayForecastEng` | `/..\component\FiveDayForecastSinhala` | `/..\component\FiveDayForecastTamil` | `/..\component\Lanuage` | `/..\component\MyCrop` | `/..\component\NewCrop` | `/..\component\News` | `/..\component\Otpverification` | `/..\component\RemoveAsset` | `/..\component\SelectCrop` | `/..\component\Selectedcrop` | `/..\component\Signin` | `/..\component\SigninSelection` | `/..\component\SignupForum` | `/..\component\Splash` | `/..\component\SuccessScreen` | `/..\component\Verify` | `/..\component\WeatherForecastEng` | `/..\component\WeatherForecastSinhala` | `/..\component\WeatherForecastTamil` | `/..\component\fixedDashboard` | `/..\component\types` | `/_sitemap`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
