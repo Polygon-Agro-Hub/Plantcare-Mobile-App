047685ad3c4ac8b0358ae87b06e47fd5a9fc7d48
R.H.Pavith Madushan Rajapaksha <pavithmadushan@gmail.com>
2024-09-30 16:27:19 +0530

    base url changed

--- environment/environment.ts
+++ environment/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
-    API_BASE_URL: 'http://10.0.2.2:3000/',
+    //API_BASE_URL: 'http://10.0.2.2:3000/',
+    API_BASE_URL: 'http://185.69.52.142:3001/',
     //TOKEN : localStorage.getItem('Login Token : ')
   };
 
