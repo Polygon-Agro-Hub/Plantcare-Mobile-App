c93c8aa672e6a14302a9e34f75f5c71ae92deaef
KHRDilshan <ravindilshan1234@gmail.com>
2025-06-24 18:07:59 +0530

    feat: add asset slice for Redux state management and update API base URL

--- services/reducxStore.ts
+++ services/reducxStore.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import packageReducer from '../store/packageSlice';
 import farmReducer from '../store/farmSlice';
 import  userReducer  from '../store/userSlice';
+import assetReducer from '../store/assetSlice'
 
 const store = configureStore({
   reducer: {
     package: packageReducer,
     farm: farmReducer,
      user: userReducer,
+     assets: assetReducer
   },
 });
 
