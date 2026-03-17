48d3a1a7ce6dac86f64ee9bf963180e40a011dbf
Avishka777 <a.rathnakumara777@gmail.com>
2026-03-17 09:35:18 +0530

    Fixed

--- environment/environment.ts
+++ environment/environment.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: true,
 
   // LOCAL --------------------
-  API_BASE_URL: "http://10.76.50.101:3000/",
+  // API_BASE_URL: "http://10.76.50.101:3000/",
 
   // DEV --------------------
-  //  API_BASE_URL: "https://dev.polygonagro.com/plantcare-api/",
+  API_BASE_URL: "https://dev.polygonagro.com/plantcare-api/",
 
   // PROD --------------------
   // API_BASE_URL: "https://polygonagro.com/plantcare-api/",
